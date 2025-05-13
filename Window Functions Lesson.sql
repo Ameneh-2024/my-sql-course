ae86c4796d8d065d2ee660a912577d8f671fb0d8
Ameneh-2024 <amenehg@yahoo.com>
2025-05-13 15:20:41 +0000

    end of course

--- Window Functions Lesson.sql
+++ Window Functions Lesson.sql
@@ -75,9 +75,9 @@ SELECT
 	, ps.AdmittedDate
 	, ps.Tariff
 	, COUNT(*) OVER () AS TotalCount
---	, COUNT(*) OVER (PARTITION BY ps.Hospital) AS HospitalCount -- create a window over those rows with the same hospital as the current row
---	, COUNT(*) OVER (PARTITION BY ps.Ward) AS WardCount
---	, COUNT(*) OVER (PARTITION BY ps.Hospital , ps.Ward) AS HospitalWardCount
+	, COUNT(*) OVER (PARTITION BY ps.Hospital) AS HospitalCount -- create a window over those rows with the same hospital as the current row
+	, COUNT(*) OVER (PARTITION BY ps.Ward) AS WardCount
+	, COUNT(*) OVER (PARTITION BY ps.Hospital , ps.Ward) AS HospitalWardCount
 FROM
 	PatientStay ps
 ORDER BY
@@ -92,9 +92,9 @@ SELECT
 	, ps.Tariff
 	, ps.Ward
 	, SUM(ps.Tariff) OVER () AS TotalTariff
---	, SUM(ps.Tariff) OVER (PARTITION BY ps.Ward) AS WardTariff
---	, 100.0 * ps.Tariff / SUM(ps.Tariff) OVER () AS PctOfAllTariff
---	, 100.0 * ps.Tariff / SUM(ps.Tariff) OVER (PARTITION BY ps.Ward) AS PctOfWardTariff
+	, SUM(ps.Tariff) OVER (PARTITION BY ps.Ward) AS WardTariff
+	, 100.0 * ps.Tariff / SUM(ps.Tariff) OVER () AS PctOfAllTariff
+	, 100.0 * ps.Tariff / SUM(ps.Tariff) OVER (PARTITION BY ps.Ward) AS PctOfWardTariff
 FROM
 	PatientStay ps
 ORDER BY
@@ -113,14 +113,15 @@ SELECT
 	, ps.Ward
 	, ps.AdmittedDate
 	, ps.Tariff
-	, ROW_NUMBER() OVER (ORDER BY ps.PatientId) AS PatientIndex
---	, ROW_NUMBER() OVER (PARTITION BY ps.Hospital ORDER BY ps.PatientId) AS PatientByHospitalIndex
---  ,COUNT(*) OVER (PARTITION BY ps.Hospital order by ps.PatientId)  as PatientByHospitalIndexAlt -- An alternative way of indexing
+	, ROW_NUMBER() OVER (ORDER BY ps.PatientId desc) AS PatientIndex
+	, ROW_NUMBER() OVER (PARTITION BY ps.Hospital ORDER BY ps.PatientId) AS PatientByHospitalIndex
+ ,COUNT(*) OVER (PARTITION BY ps.Hospital order by ps.PatientId)  as PatientByHospitalIndexAlt -- An alternative way of indexing
 FROM
 	PatientStay ps
 ORDER BY
 	ps.Hospital
-	, ps.PatientId;
+, 
+	ps.PatientId;
 
 /*
 Compare ROW_NUMBER(), RANK() and DENSE_RANK() where there are ties
@@ -134,8 +135,8 @@ SELECT
 	ps.PatientId
 	, ps.Tariff
 	, ROW_NUMBER() OVER (ORDER BY ps.Tariff DESC) AS PatientRowIndex
---	, RANK() OVER (	ORDER BY ps.Tariff DESC) AS PatientRank
---	, DENSE_RANK() OVER (ORDER BY ps.Tariff DESC) AS PatientDenseRank
+	, RANK() OVER (	ORDER BY ps.Tariff DESC) AS PatientRank
+	, DENSE_RANK() OVER (ORDER BY ps.Tariff DESC) AS PatientDenseRank
 --	, NTILE(10) OVER (ORDER BY ps.Tariff DESC) AS PatientIdDecile
 FROM
 	PatientStay ps
